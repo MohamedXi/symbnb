6d7529ce80a9da7463599e3f6c152e26aee6f638
Ismael MOHAMED <ismaelmohamed@ismaels-macbook-pro.home>
2019-04-20 13:52:51 +0200

    build: add facker and slugify library

--- src/DataFixtures/AdFixtures.php
+++ src/DataFixtures/AdFixtures.php
@@ -3,21 +3,33 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AdFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('FR-fr');
+        $slugify = new Slugify();
+
         // For the content in the database
         for ($i = 1; $i <= 30; $i++) {
             $ad = new Ad(); // Ad variable
-            $ad->setTitle("Ad title n°$i")
-                ->setSlug("ad-title-n-$i")
-                ->setCoverImage("http://placehold.it/1000x300")
-                ->setIntroduction("Hello everybody, this an introduction")
-                ->setContent("<p>I'm a rich content</p>")
+
+            $title = $faker->sentence();
+            $slug = $slugify->slugify($title);
+            $coverImage = $faker->imageUrl('1000, 350');
+            $intro = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>', $faker-> paragraphs(5)) . '</p>';
+
+            $ad->setTitle($title)
+                ->setSlug($slug)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($intro)
+                ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
                 ->setRooms(mt_rand(1, 5));
             $manager->persist($ad); // Persist the content
